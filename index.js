f8dfdc7263d03c5781e4da93ddd9e7ad8c908ba7
KhulupArtemy <artem.khulup@gmail.com>
2023-11-07 15:27:52 +0300

    Email functions for leasing and tenders

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+})
+
 app.use(express.json())
 
 const transporter = nodemailer.createTransport({
@@ -16,21 +23,73 @@ const transporter = nodemailer.createTransport({
     }
 })
 
-app.post('/sendEmail', async (req, res) => {
-    let {initials, email, phone, good, model, comment} = req.body;
+app.post('/api/sendEmail/goods', async (req, res) => {
+    try {
+        let {initials, email, phone, good, model, comment} = req.body;
 
-    if (comment) {
-        comment = '\nКомментарий пользователя: ' + comment + '.'
+        if (comment) {
+            comment = '\nКомментарий пользователя: ' + comment + '.'
+        }
+
+        const mailOptions = {
+            from: 'site.prioritet@gmail.com',
+            to: 'info@tc-prioritet.ru',
+            subject: 'Запрос на технику',
+            text: `Пользователь ${initials} сделал запрос на ${good} ${model}.\nПочта: ${email}.\nТелефон: ${phone}. ${comment}`
+        }
+
+        transporter.sendMail(mailOptions)
+
+        return res.json('Заявка отправлена!')
+    } catch (e) {
+        return res.json(e.message)
     }
+})
 
-    const mailOptions = {
-        from: 'site.prioritet@gmail.com',
-        to: 'info@tc-prioritet.ru',
-        subject: 'Запрос на технику',
-        text: `Пользователь ${initials} сделал запрос на ${good} ${model}.\nПочта: ${email}.\nТелефон: ${phone}. ${comment}`
+app.post('/api/sendEmail/leasing', async (req, res) => {
+    try {
+        let {initials, email, phone, comment} = req.body;
+
+        if (comment) {
+            comment = '\nКомментарий пользователя: ' + comment + '.'
+        }
+
+        const mailOptions = {
+            from: 'site.prioritet@gmail.com',
+            to: 'info@tc-prioritet.ru',
+            subject: 'Лизинг',
+            text: `Пользователь ${initials}.\nПочта: ${email}.\nТелефон: ${phone}. ${comment}`
+        }
+
+        transporter.sendMail(mailOptions)
+
+        return res.json('Заявка отправлена!')
+    } catch (e) {
+        return res.json(e.message)
     }
+})
 
-    transporter.sendMail(mailOptions)
+app.post('/api/sendEmail/tenders', async (req, res) => {
+    try {
+        let {initials, email, phone, comment} = req.body;
+
+        if (comment) {
+            comment = '\nКомментарий пользователя: ' + comment + '.'
+        }
+
+        const mailOptions = {
+            from: 'site.prioritet@gmail.com',
+            to: 'info@tc-prioritet.ru',
+            subject: 'Тендер',
+            text: `Пользователь ${initials}.\nПочта: ${email}.\nТелефон: ${phone}. ${comment}`
+        }
+
+        transporter.sendMail(mailOptions)
+
+        return res.json('Заявка отправлена!')
+    } catch (e) {
+        return res.json(e.message)
+    }
 })
 
 const start = async () => {
